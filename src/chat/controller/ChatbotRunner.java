d7e92721e63213d75cf8eeb1f25f58b3200cb9b1
noahrb <nbev0918@740s-pl-127.local>
2016-10-14 12:35:56 -0600

    Created project, added controller model and view and associated classes with runner. Added repo settings

---
+++ src/chat/controller/ChatbotRunner.java
@@ -0,0 +1,11 @@
+package chat.controller;
+
+public class ChatbotRunner
+{
+	public static void main (String [] args)
+	{
+		ChatbotController chatbotAppController = new ChatbotController();
+		chatbotAppController.start();
+	}	
+}
+	
