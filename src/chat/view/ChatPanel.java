98ab72f5e2df90a3a312ff4776d48cb87e03c8ee
noahrb <nbev0918@740s-pl-127.local>
2016-11-17 11:30:39 -0700

    Added picture and added panel and fixed frame

--- src/chat/view/ChatPanel.java
+++ src/chat/view/ChatPanel.java
@@ -21,60 +21,45 @@ public class ChatPanel extends JPanel
 		baseLayout = new SpringLayout();
 		chatDisplay = new JTextArea(5, 25);
 		chatField = new JTextField(25);
-<<<<<<< HEAD
-		chatButton = new JButton("Chat with the Bot");
-=======
-		baseLayout.putConstraint(SpringLayout.SOUTH, chatField, -174, SpringLayout.SOUTH, this);
-		baseLayout.putConstraint(SpringLayout.NORTH, chatDisplay, 32, SpringLayout.SOUTH, chatField);
-		baseLayout.putConstraint(SpringLayout.EAST, chatDisplay, 0, SpringLayout.EAST, chatField);
-		baseLayout.putConstraint(SpringLayout.WEST, chatField, 65, SpringLayout.WEST, this);
-		chatButton = new JButton("Chat with the Bot");
-		baseLayout.putConstraint(SpringLayout.SOUTH, chatButton, -6, SpringLayout.NORTH, chatField);
-		baseLayout.putConstraint(SpringLayout.EAST, chatButton, -148, SpringLayout.EAST, this);
-		
->>>>>>> master
+		chatButton = new JButton("Chat with the bot");
 		setupPanel();
 		setupLayout();
 		setupListeners();
+		
 	}
 	
 	private void setupPanel()
 	{
 		this.setLayout(baseLayout);
-		this.setBackground(Color.GREEN);
-		this.add(chatDisplay);
+		this.setBackground(Color.GRAY);
 		this.add(chatButton);
 		this.add(chatField);
+		this.add(chatDisplay);
 	}
 	
 	private void setupLayout()
 	{
-<<<<<<< HEAD
-		baseLayout.putConstraint(SpringLayout.SOUTH, chatButton, -6, SpringLayout.NORTH, chatField);
-		baseLayout.putConstraint(SpringLayout.EAST, chatButton, -148, SpringLayout.EAST, this);
-		baseLayout.putConstraint(SpringLayout.SOUTH, chatField, -174, SpringLayout.SOUTH, this);
-		baseLayout.putConstraint(SpringLayout.NORTH, chatDisplay, 32, SpringLayout.SOUTH, chatField);
-		baseLayout.putConstraint(SpringLayout.EAST, chatDisplay, 0, SpringLayout.EAST, chatField);
-		baseLayout.putConstraint(SpringLayout.WEST, chatField, 65, SpringLayout.WEST, this);
-=======
-		
->>>>>>> master
+		baseLayout.putConstraint(SpringLayout.NORTH, chatDisplay, 65, SpringLayout.NORTH, this);
+		baseLayout.putConstraint(SpringLayout.WEST, chatDisplay, 75, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.WEST, chatField, 68, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.SOUTH, chatField, -56, SpringLayout.SOUTH, this);
+		baseLayout.putConstraint(SpringLayout.EAST, chatField, -68, SpringLayout.EAST, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, chatButton, 6, SpringLayout.SOUTH, chatField);
+		baseLayout.putConstraint(SpringLayout.WEST, chatButton, 148, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.SOUTH, chatButton, -21, SpringLayout.SOUTH, this);
+		baseLayout.putConstraint(SpringLayout.EAST, chatButton, -149, SpringLayout.EAST, this);
 	}
 	
 	private void setupListeners()
 	{
 		chatButton.addActionListener(new ActionListener()
 		{
-			public void actionPerformed(ActionEvent click)
-			{
+			public void actionPerformed(ActionEvent click){
 				String userWords = chatField.getText();
 				String botResponse = baseController.useChatbotCheckers(userWords);
 				
 				chatDisplay.setText("You said: " + userWords + "\n" + "Chatbot said: " + botResponse);
-<<<<<<< HEAD
 				chatField.setText("");
-=======
->>>>>>> master
 			}
 		});
 	}
